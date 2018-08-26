e0884112eaabe7c2d3b8c8c3b3d6a10337d1497b
EKibet <kibetedgar@gmail.com>
2018-08-26 16:10:37 +0300

    adds credentials production code

---
+++ credentials.py
@@ -0,0 +1,12 @@
+import sys
+
+
+class Credentials():
+    credentials_list = []
+    def __init__(self,site_name,site_username,site_password):
+        self.site_name = site_name
+        self.site_username = site_username
+        self.site_password = site_password
+
+    def save_credential(self):
+        self.credentials_list.append(self)
