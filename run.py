0cc353f21a51c79230c30432496913d9b3d86955
EKibet <kibetedgar@gmail.com>
2018-08-26 15:11:25 +0300

    reviews while loop

--- run.py
+++ run.py
@@ -7,13 +7,18 @@ from format_outputs import *
 def create_acc(u_name, u_pass):
     # u_pass= hashlib.new(u_pass)
     # encrypted_pass=sdigest(u_pass)
-    new_acc =Users(u_name,u_pass)
-    return new_acc
+    if u_name != " " or u_pass != " ":
+        new_acc =Users(u_name,u_pass)
+        return new_acc
+    else:
+        print_e("Cannot save empty fields!")
+        
 
 def save_acc_info(accout):
     accout.save_user()
 
 credentials_message="\t\t\tBeware that the CM is case sensitive!"
+
 def main():
     while True:
         print("Use these short code :\n\tCA - Create Acount,\n\tLI - Log in")
@@ -29,11 +34,10 @@ def main():
             print ("\tEnter Password:")
             u_pass = getpass.getpass("\t")
             save_acc_info(create_acc(u_name,u_pass))
-            print_s(f"\t\t{u_name} Account Successfully created\n")
-
+            print_s(f"\t\t{u_name} account Successfully created\n")
         else:
-                print("User does not exist")
-                break
+            print("User does not exist")
+            break
     else:
         print("Enter the specified options")
         # break
