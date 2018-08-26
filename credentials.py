cbd881cd015a96cae9d15097320c9049ae717545
EKibet <kibetedgar@gmail.com>
2018-08-26 20:38:09 +0300

    reviews credentials find and display fns

--- credentials.py
+++ credentials.py
@@ -2,17 +2,26 @@ import sys
 
 
 class Credentials():
-    credentials_list = []
+    credentials_list = {}
     def __init__(self,site_name,site_username,site_password):
         self.site_name = site_name
         self.site_username = site_username
         self.site_password = site_password
 
     def save_credential(self):
-        self.credentials_list.append(self)
+        user_data = {
+            site_name
+        }
+        self.credentials_list.push(self)
 
     
     @classmethod
     def display_credentials(cls):
         for credential in cls.credentials_list:
             return credential
+
+    @classmethod
+    def find_by_sitename(cls,search):
+        for credentials in cls.credentials_list:
+            if credentials.site_name == search:
+                return credentials
