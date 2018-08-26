8ddafa85626ab9114f69857707fa3a326b1fd133
EKibet <kibetedgar@gmail.com>
2018-08-26 20:37:13 +0300

    Updates format class

--- format_outputs.py
+++ format_outputs.py
@@ -5,10 +5,10 @@ class FormatOutput():
     colors = {
         'success': '\033[92m',
         'warning': '\033[91m',
-        'default': '\033[99m',
+        'defalt': '\033[99m',
         'blue': '\033[94m',
         'fail': '\033[91m',
-        'end': '\033[0m',
+        'default': '\033[0m',
         'yellow': '\033[93m',
         'magenta': '\033[95m'
     }
