d0035f00ef4aa42f2d41b3854ba76ad89a4ac1de
EKibet <kibetedgar@gmail.com>
2018-08-26 15:19:47 +0300

    adds log in verify user

--- run.py
+++ run.py
@@ -12,7 +12,12 @@ def create_acc(u_name, u_pass):
         return new_acc
     else:
         print_e("Cannot save empty fields!")
-        
+
+def verify_user(uname,upass):
+	'''
+	Function that verifies the existance of the user before creating credentials
+	'''
+	checking_user = Users.check_user(uname,upass)
 
 def save_acc_info(accout):
     accout.save_user()
@@ -37,18 +42,14 @@ def main():
             print_s(f"\t\t{u_name} account Successfully created\n")
         
         elif short_code == 'LI':
-            print_w(credentials_message)
-            print_w (credentials_message)
-            print("-"*10)
-
-            print ("\tEnter Username: ")
-            u_name = input("\t")
-
-            print ("\tEnter Password:")
-            u_pass = getpass.getpass("\t")
-            save_acc_info(create_acc(u_name,u_pass))
-            print_s(f"\t\t{u_name} Account Successfully created\n")
-
+            print(credentials_message)
+            print ("\tEnter Your Username: ")
+            uname = input("\t\t")
+            print("\tEnter your Password: ")
+            upass = getpass.getpass("\t\t")
+            user_exists = verify_user(uname,upass)
+            if user_exists == uname:
+                print_s("Welcome!")
         
         else:
             print("User does not exist")
